694dc80996c09648de73df972de086eaa5aa9585
wangminan <wangminan0811@hotmail.com>
2025-03-13 16:20:10 +0800

    【FIX】修改命名

--- data-model-human-machine/src/main/java/org/datacenter/model/flight/Sorties.java
+++ data-model-human-machine/src/main/java/org/datacenter/model/flight/Sorties.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Flight {
+public class Sorties {
     /**
      * 机型
      */
--- data-model-human-machine/src/main/java/org/datacenter/model/flight/SortiesBatch.java
+++ data-model-human-machine/src/main/java/org/datacenter/model/flight/SortiesBatch.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class FlightBatch {
+public class SortiesBatch {
     /**
      * 主键ID
      */
